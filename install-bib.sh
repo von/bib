12fd3cf9b1e1cc54e08729f1376c66a91d8d9d3c
Von Welch <von@vwelch.com>
2012-06-30 07:03:45 -0400

    install-bib.sh: Just copy bib to dev directory (not www host)

--- install-bib.sh
+++ install-bib.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 #
 # Install a newly created bib.htm into my website development directory
-# as well as production and test websites.
 
 # Exit on any error.
 set -e
 
 BIB_HTML="bib.html"
 
-WEB_HOST="vwelch.com"
-PRODUCTION_WEB_TARGET="www.vonwelch.com/pubs/index.html"
-TEST_WEB_TARGET="test.vonwelch.com/pubs/index.html"
-
 DEV_ROOT="${HOME}/develop/www/"
-PRODUCTION_DEV_PATH="${DEV_ROOT}/www.vonwelch.com/pubs/index.html"
-TEST_DEV_PATH="${DEV_ROOT}/test.vonwelch.com/pubs/index.html"
+DEV_PATH="${DEV_ROOT}/www.vonwelch.com/pubs/index.html"
 
 
 if test -f ${BIB_HTML} ; then
@@ -24,14 +18,9 @@ else
     exit 1
 fi
 
-for dev_file in ${PRODUCTION_DEV_PATH} ${TEST_DEV_PATH} ; do
+for dev_file in ${DEV_PATH} ; do
     echo "Copying ${BIB_HTML} to ${dev_file}"
     cp ${BIB_HTML} ${dev_file}
-done
-
-for web_file in ${PRODUCTION_WEB_TARGET} ${TEST_WEB_TARGET} ; do 
-    echo "SCP'ing ${BIB_HTML} to ${WEB_HOST}:${web_file}"
-    scp ${BIB_HTML} ${WEB_HOST}:${web_file}
 done
 
 echo "Success"
